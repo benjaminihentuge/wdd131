d53ace96af77e76fc28dc0f097485bfa5720b88b
benjaminihentuge <benjaminihentuge2017@gmail.com>
2026-02-09 08:57:28 +0100

    week05-07

--- week05/scripts/review.js
+++ week05/scripts/review.js
@@ -3,3 +3,6 @@ reviewCount++;
 localStorage.setItem("reviewCount", reviewCount);
 
 document.querySelector("#count").textContent = reviewCount;
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent =
+  `Last modified: ${document.lastModified}`;
